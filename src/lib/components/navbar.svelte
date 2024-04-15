5387937dbafaaf0abc75f0a4c286eadaeabb017b
Aydin Elhan <200030355@student.kangan.edu.au>
2024-04-16 09:41:09 +1000

    first commit

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-row items-center justify-between p-4">
+	<a href="/"> <button class="btn variant-filled-surface rounded">Home</button></a>
+	<div class="space-x-2">
+		<a href="/logout"><button class="btn variant-filled-secondary rounded">Log out</button></a>
+		<a href="/login"><button class="btn variant-filled-primary rounded">Log in</button></a>
+	</div>
+</div>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,3 @@
+<div class="h-[80vh] flex items-center justify-center">
+	<h1 class="text-4xl font-bold">Dashboard Page</h1>
+</div>
