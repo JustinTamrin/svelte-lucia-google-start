c8c5f8aa9dc86d61d16ef4c829675888cf6b14df
Justin Nicholas Tamrin <justintamrin888@gmail.com>
2024-11-02 00:21:46 +0900

    OAuth Done

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,13 @@
+import type { PrismaClient } from '@prisma/client/extension';
+
 declare global {
 	namespace App {
-		
+		interface Locals {
+			user: import('lucia').User | null;
+			session: import('lucia').Session | null;
+		}
 	}
+	var prisma: PrismaClient;
 }
+
 export {};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,7 +6,7 @@
 	<div class="flex flex-col space-y-6 p-12">
 		<p class="text-2xl">Log in to Your Account</p>
 		<div class="grid grid-cols-1 gap-6">
-			<a href="/">
+			<a href="/login/google">
 				<button class="w-full btn variant-filled-primary rounded">
 					<Google class="mr-2 h-4 w-4" />
 					Google
